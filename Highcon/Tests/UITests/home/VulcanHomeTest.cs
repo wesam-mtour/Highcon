10d4d7be3eca94fb853d8991390b3f4409438b4e
wesam.mtour <wesam.mtour@nutanix.com>
2023-02-21 21:18:57 +0200

    v1

---
+++ Highcon/Tests/UITests/home/VulcanHomeTest.cs
@@ -0,0 +1,35 @@
+﻿using Highcon.webdriverinitializer;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace Highcon.Tests.UITests.home
+{
+    public class VulcanHomeTest
+    {
+        private WebDriver webDriver;
+        String WEB_DRIVER_URL = "https://integration.vulcan.highcon.link/queue";
+
+        [SetUp]
+        public void start_Browser()
+        {
+            webDriver = BrowserFactory.StartWebDriver("Chrome");
+            BrowserFactory.OpenUrl(WEB_DRIVER_URL);
+        }
+        [Test]
+        public void test_Browserops()
+        {
+            System.Threading.Thread.Sleep(4000);
+            IWebElement element = webDriver.FindElement(By.CssSelector("p#headerTitle"));
+            Console.WriteLine(element.Text);
+            // Test verification from the header title which should be equal to Job Queue
+            Assert.AreEqual(element.Text, "Job Queue");
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            BrowserFactory.Close();
+        }
+    }
+}
---
+++ Highcon/webdriverinitializer/BrowserFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Highcon.webdriverinitializer
+{
+    public class BrowserFactory
+    {
+        private static WebDriver webDriver;
+        public static WebDriver StartWebDriver(String browserName){
+            switch (browserName.ToLower())
+            {
+                case "chrome":
+                    webDriver = new ChromeDriver();
+                    webDriver.Manage().Window.Maximize();
+                    break;                    
+            }
+            return (WebDriver)webDriver;
+        }
+        public static string Title
+        {
+            get { return webDriver.Title; }
+        }
+        public static void OpenUrl(string url)
+        {
+            webDriver.Url = url;
+        }
+        public static WebDriver getDriver
+        {
+            get { return webDriver; }
+        }
+        public static void Close()
+        {
+            webDriver.Quit();
+        }
+    }
+}
